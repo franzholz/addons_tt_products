809d261ffecff8931cd218af5ecbba74bf2735e1
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-30 14:46:48 +0000

    initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/addons_tt_products/trunk@15342 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ README.txt
@@ -0,0 +1,16 @@
+You can use these example templates and icons for tt_products 2.8.0 or higher.
+
+Get the next versions of tt_products from this online shop:
+http://ttproducts.de
+
+pob_ttp_marker_xml.htm: multiple language marker template sponsored by Rainer Lindner (pob.com)
+products_css_de.html: CSS template by Robert Markula and translated by Masod Mohmand
+
+The folder meerwijn contains the shop sources for the site http://meerwijn.nl .
+
+Please join the team to further develop these templates at http://forge.typo3.org .
+Send us your shop templates and icons to collect them here.
+
+Regards,
+
+Franz Holzinger
---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "addons_tt_products"
+#
+# Auto generated 29-12-2008 20:15
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Addons for tt_products',
+	'description' => 'Additional example templates and icons for the tt_products shop system version 2.8.0+.',
+	'category' => 'example',
+	'author' => 'Franz Holzinger',
+	'author_email' => 'franz@ttproducts.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'jambage.com',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:55:{s:9:"ChangeLog";s:4:"0fa9";s:10:"README.txt";s:4:"482a";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"2337";s:20:"doc/wizard_form.html";s:4:"796d";s:24:"res/icons/fe/AddItem.gif";s:4:"287d";s:34:"res/icons/fe/Cart-Icon-AddItem.gif";s:4:"e76c";s:40:"res/icons/fe/Cart-Icon-AddRemoveItem.gif";s:4:"9b18";s:37:"res/icons/fe/Cart-Icon-RemoveItem.gif";s:4:"b9cc";s:26:"res/icons/fe/Cart-Icon.gif";s:4:"988a";s:27:"res/icons/fe/RemoveItem.gif";s:4:"e28f";s:23:"res/icons/fe/basket.gif";s:4:"ca3d";s:25:"template/TS_setup_TTP.txt";s:4:"acc7";s:31:"template/pob_ttp_marker_xml.htm";s:4:"dd8c";s:29:"template/products_css_de.html";s:4:"0744";s:29:"template/products_css_en.html";s:4:"7dc1";s:25:"template/products_de.html";s:4:"d02a";s:31:"template/products_template.tmpl";s:4:"6916";s:34:"template/products_template_dk.tmpl";s:4:"ab84";s:34:"template/products_template_fi.tmpl";s:4:"7c85";s:34:"template/products_template_fr.tmpl";s:4:"bf0c";s:40:"template/products_template_htmlmail.tmpl";s:4:"aa8a";s:34:"template/products_template_it.html";s:4:"30e6";s:34:"template/products_template_se.tmpl";s:4:"82d8";s:24:"template/tt_products.css";s:4:"54ea";s:32:"template/tt_products_example.css";s:4:"ca34";s:37:"template/tt_products_example_css.html";s:4:"ee8b";s:14:"meerwijn/+.gif";s:4:"36a3";s:14:"meerwijn/_.gif";s:4:"209f";s:19:"meerwijn/basket.gif";s:4:"508c";s:30:"meerwijn/detail_cadeaubon.tmpl";s:4:"c263";s:31:"meerwijn/detail_geschenken.tmpl";s:4:"b695";s:31:"meerwijn/detail_kurkenshop.tmpl";s:4:"0fad";s:29:"meerwijn/detail_shopabox.tmpl";s:4:"21a3";s:27:"meerwijn/detail_wijnen.tmpl";s:4:"63be";s:28:"meerwijn/help-icon-hover.gif";s:4:"05b8";s:22:"meerwijn/help-icon.gif";s:4:"8f01";s:28:"meerwijn/info-icon-hover.gif";s:4:"fdbf";s:22:"meerwijn/info-icon.gif";s:4:"271c";s:17:"meerwijn/kurk.gif";s:4:"c7fb";s:17:"meerwijn/lock.gif";s:4:"80ca";s:28:"meerwijn/product_detail.tmpl";s:4:"9e4a";s:36:"meerwijn/product_proefpakketten.tmpl";s:4:"9afd";s:23:"meerwijn/producten.tmpl";s:4:"103a";s:24:"meerwijn/recycle-bin.gif";s:4:"b8a7";s:24:"meerwijn/shop-a-box.tmpl";s:4:"f580";s:31:"meerwijn/totaal_geschenken.tmpl";s:4:"15ca";s:31:"meerwijn/totaal_kurkenshop.tmpl";s:4:"1306";s:29:"meerwijn/totaal_shopabox.tmpl";s:4:"f87b";s:27:"meerwijn/totaal_wijnen.tmpl";s:4:"5ee1";s:22:"meerwijn/tracking.tmpl";s:4:"aadb";s:19:"meerwijn/unlock.gif";s:4:"12c9";s:25:"meerwijn/winkelwagen.tmpl";s:4:"ff1b";s:14:"meerwijn/x.gif";s:4:"1ccf";s:26:"meerwijn/js/FormManager.js";s:4:"3ccc";}',
+	'suggests' => array(
+	),
+);
+
+?>
