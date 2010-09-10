90c58d7d8ef0a5e9ba47ba5c98d42f63de745e55
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-10 14:19:13 +0000

    modifications to example_template_bill_de.tmpl and tt_products_example_css.html
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/addons_tt_products/trunk@37954 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,18 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "addons_tt_products"
+# Extension Manager/Repository config file for ext "addons_tt_products".
 #
-# Auto generated 11-08-2009 18:22
+# Auto generated 10-06-2010 07:20
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Addons to tt_products',
-	'description' => 'Additional example templates, multilingual marker translations and icons for the tt_products shop system version 2.8.0+.',
+	'description' => 'Many translations of standard texts for shops. Additional example templates and icons for the tt_products shop system version 2.8.0+.',
 	'category' => 'example',
 	'author' => 'Franz Holzinger',
 	'author_email' => 'franz@ttproducts.de',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'demo.ttproducts.de',
-	'version' => '0.2.1',
+	'version' => '0.2.4',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:65:{s:9:"ChangeLog";s:4:"ddae";s:10:"README.txt";s:4:"482a";s:16:"en.locallang.xml";s:4:"e83e";s:16:"es.locallang.xml";s:4:"ba4e";s:12:"ext_icon.gif";s:4:"1bdc";s:16:"fr.locallang.xml";s:4:"ff95";s:16:"hu.locallang.xml";s:4:"3850";s:16:"mk.locallang.xml";s:4:"c902";s:16:"nl.locallang.xml";s:4:"2991";s:16:"ru.locallang.xml";s:4:"4d66";s:24:"res/icons/fe/AddItem.gif";s:4:"287d";s:34:"res/icons/fe/Cart-Icon-AddItem.gif";s:4:"e76c";s:40:"res/icons/fe/Cart-Icon-AddRemoveItem.gif";s:4:"9b18";s:37:"res/icons/fe/Cart-Icon-RemoveItem.gif";s:4:"b9cc";s:26:"res/icons/fe/Cart-Icon.gif";s:4:"988a";s:27:"res/icons/fe/RemoveItem.gif";s:4:"e28f";s:23:"res/icons/fe/basket.gif";s:4:"ca3d";s:25:"template/TS_setup_TTP.txt";s:4:"9f6c";s:16:"template/agb.txt";s:4:"d76f";s:38:"template/example_template_bill_de.tmpl";s:4:"2b5e";s:35:"template/payment_DIBS_template.tmpl";s:4:"4ff6";s:38:"template/payment_DIBS_template_uk.tmpl";s:4:"3a94";s:24:"template/paymentlib.tmpl";s:4:"bc9a";s:31:"template/pob_ttp_marker_xml.htm";s:4:"7ea4";s:29:"template/products_css_de.html";s:4:"0b9d";s:29:"template/products_css_en.html";s:4:"7f2a";s:25:"template/products_de.html";s:4:"dce0";s:31:"template/products_template.tmpl";s:4:"6916";s:34:"template/products_template_dk.tmpl";s:4:"f9db";s:34:"template/products_template_fi.tmpl";s:4:"9850";s:34:"template/products_template_fr.tmpl";s:4:"e8e4";s:40:"template/products_template_htmlmail.tmpl";s:4:"aa8a";s:34:"template/products_template_it.html";s:4:"30e6";s:34:"template/products_template_se.tmpl";s:4:"477a";s:24:"template/tt_products.css";s:4:"54ea";s:32:"template/tt_products_example.css";s:4:"244c";s:37:"template/tt_products_example_css.html";s:4:"d42a";s:14:"meerwijn/+.gif";s:4:"36a3";s:14:"meerwijn/_.gif";s:4:"209f";s:19:"meerwijn/basket.gif";s:4:"508c";s:30:"meerwijn/detail_cadeaubon.tmpl";s:4:"c263";s:31:"meerwijn/detail_geschenken.tmpl";s:4:"b695";s:31:"meerwijn/detail_kurkenshop.tmpl";s:4:"0fad";s:29:"meerwijn/detail_shopabox.tmpl";s:4:"21a3";s:27:"meerwijn/detail_wijnen.tmpl";s:4:"63be";s:28:"meerwijn/help-icon-hover.gif";s:4:"05b8";s:22:"meerwijn/help-icon.gif";s:4:"8f01";s:28:"meerwijn/info-icon-hover.gif";s:4:"fdbf";s:22:"meerwijn/info-icon.gif";s:4:"271c";s:17:"meerwijn/kurk.gif";s:4:"c7fb";s:17:"meerwijn/lock.gif";s:4:"80ca";s:28:"meerwijn/product_detail.tmpl";s:4:"9e4a";s:36:"meerwijn/product_proefpakketten.tmpl";s:4:"9afd";s:23:"meerwijn/producten.tmpl";s:4:"103a";s:24:"meerwijn/recycle-bin.gif";s:4:"b8a7";s:24:"meerwijn/shop-a-box.tmpl";s:4:"f580";s:31:"meerwijn/totaal_geschenken.tmpl";s:4:"15ca";s:31:"meerwijn/totaal_kurkenshop.tmpl";s:4:"1306";s:29:"meerwijn/totaal_shopabox.tmpl";s:4:"f87b";s:27:"meerwijn/totaal_wijnen.tmpl";s:4:"5ee1";s:22:"meerwijn/tracking.tmpl";s:4:"aadb";s:19:"meerwijn/unlock.gif";s:4:"12c9";s:25:"meerwijn/winkelwagen.tmpl";s:4:"ff1b";s:14:"meerwijn/x.gif";s:4:"1ccf";s:26:"meerwijn/js/FormManager.js";s:4:"3ccc";}',
+	'_md5_values_when_last_written' => 'a:78:{s:9:"ChangeLog";s:4:"8c5a";s:10:"README.txt";s:4:"482a";s:16:"en.locallang.xml";s:4:"e83e";s:16:"es.locallang.xml";s:4:"ba4e";s:12:"ext_icon.gif";s:4:"38fc";s:17:"ext_localconf.php";s:4:"fc72";s:16:"fr.locallang.xml";s:4:"552a";s:16:"hu.locallang.xml";s:4:"3850";s:16:"mk.locallang.xml";s:4:"c902";s:16:"nl.locallang.xml";s:4:"2991";s:16:"ru.locallang.xml";s:4:"4d66";s:14:"meerwijn/+.gif";s:4:"36a3";s:14:"meerwijn/_.gif";s:4:"209f";s:19:"meerwijn/basket.gif";s:4:"508c";s:30:"meerwijn/detail_cadeaubon.tmpl";s:4:"c263";s:31:"meerwijn/detail_geschenken.tmpl";s:4:"b695";s:31:"meerwijn/detail_kurkenshop.tmpl";s:4:"0fad";s:29:"meerwijn/detail_shopabox.tmpl";s:4:"21a3";s:27:"meerwijn/detail_wijnen.tmpl";s:4:"63be";s:28:"meerwijn/help-icon-hover.gif";s:4:"05b8";s:22:"meerwijn/help-icon.gif";s:4:"8f01";s:28:"meerwijn/info-icon-hover.gif";s:4:"fdbf";s:22:"meerwijn/info-icon.gif";s:4:"271c";s:17:"meerwijn/kurk.gif";s:4:"c7fb";s:17:"meerwijn/lock.gif";s:4:"80ca";s:28:"meerwijn/product_detail.tmpl";s:4:"9e4a";s:36:"meerwijn/product_proefpakketten.tmpl";s:4:"9afd";s:23:"meerwijn/producten.tmpl";s:4:"103a";s:24:"meerwijn/recycle-bin.gif";s:4:"b8a7";s:24:"meerwijn/shop-a-box.tmpl";s:4:"f580";s:31:"meerwijn/totaal_geschenken.tmpl";s:4:"15ca";s:31:"meerwijn/totaal_kurkenshop.tmpl";s:4:"1306";s:29:"meerwijn/totaal_shopabox.tmpl";s:4:"f87b";s:27:"meerwijn/totaal_wijnen.tmpl";s:4:"5ee1";s:22:"meerwijn/tracking.tmpl";s:4:"aadb";s:19:"meerwijn/unlock.gif";s:4:"12c9";s:25:"meerwijn/winkelwagen.tmpl";s:4:"ff1b";s:14:"meerwijn/x.gif";s:4:"1ccf";s:26:"meerwijn/js/FormManager.js";s:4:"3ccc";s:18:"res/icons/fe/+.gif";s:4:"36a3";s:24:"res/icons/fe/AddItem.gif";s:4:"287d";s:34:"res/icons/fe/Cart-Icon-AddItem.gif";s:4:"e76c";s:40:"res/icons/fe/Cart-Icon-AddRemoveItem.gif";s:4:"9b18";s:37:"res/icons/fe/Cart-Icon-RemoveItem.gif";s:4:"b9cc";s:26:"res/icons/fe/Cart-Icon.gif";s:4:"988a";s:27:"res/icons/fe/RemoveItem.gif";s:4:"e28f";s:18:"res/icons/fe/_.gif";s:4:"209f";s:28:"res/icons/fe/basket-grey.gif";s:4:"ca3d";s:23:"res/icons/fe/basket.gif";s:4:"508c";s:32:"res/icons/fe/help-icon-hover.gif";s:4:"05b8";s:26:"res/icons/fe/help-icon.gif";s:4:"8f01";s:32:"res/icons/fe/info-icon-hover.gif";s:4:"fdbf";s:26:"res/icons/fe/info-icon.gif";s:4:"271c";s:21:"res/icons/fe/kurk.gif";s:4:"c7fb";s:21:"res/icons/fe/lock.gif";s:4:"80ca";s:28:"res/icons/fe/recycle-bin.gif";s:4:"b8a7";s:23:"res/icons/fe/unlock.gif";s:4:"12c9";s:18:"res/icons/fe/x.gif";s:4:"1ccf";s:25:"template/TS_setup_TTP.txt";s:4:"9f6c";s:16:"template/agb.txt";s:4:"d76f";s:38:"template/example_template_bill_de.tmpl";s:4:"33c6";s:35:"template/payment_DIBS_template.tmpl";s:4:"4ff6";s:38:"template/payment_DIBS_template_uk.tmpl";s:4:"3a94";s:24:"template/paymentlib.tmpl";s:4:"dde6";s:31:"template/pob_ttp_marker_xml.htm";s:4:"b514";s:29:"template/products_css_de.html";s:4:"088d";s:29:"template/products_css_en.html";s:4:"7951";s:25:"template/products_de.html";s:4:"d17c";s:31:"template/products_template.tmpl";s:4:"6916";s:34:"template/products_template_dk.tmpl";s:4:"f9db";s:34:"template/products_template_fi.tmpl";s:4:"9850";s:34:"template/products_template_fr.tmpl";s:4:"e8e4";s:40:"template/products_template_htmlmail.tmpl";s:4:"aa8a";s:34:"template/products_template_it.html";s:4:"30e6";s:34:"template/products_template_se.tmpl";s:4:"477a";s:24:"template/tt_products.css";s:4:"54ea";s:32:"template/tt_products_example.css";s:4:"244c";s:37:"template/tt_products_example_css.html";s:4:"6eec";}',
 	'suggests' => array(
 	),
 );
