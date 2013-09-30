58a8f50ee8552293fca2adb4d5fa5e99762c1bde
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-30 13:00:07 +0000

    version 0.3.0 from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/addons_tt_products/trunk@80207 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Addons to tt_products',
-	'description' => 'Many translations of standard texts for shops. Additional example templates and icons for the tt_products shop system version 2.8.0+.',
+	'description' => 'Many translations of standard texts for shops. Additional example templates and icons for the tt_products shop system version 2.9+.',
 	'category' => 'example',
 	'author' => 'Franz Holzinger',
 	'author_email' => 'franz@ttproducts.de',
@@ -28,10 +28,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author_company' => 'demo.ttproducts.de',
-	'version' => '0.2.13',
+	'author_company' => 'ttproducts.org',
+	'version' => '0.3.0',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.3.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
