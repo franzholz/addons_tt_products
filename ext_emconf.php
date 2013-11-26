aac923a1615076db7dcf5fe63a578fae43494c32
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-26 11:38:24 +0000

    * add IBAN marker ###PERSON_ACCOUNTS_IBAN### for SEPA * fix bugs in template tt_products_example_css.html * add new subpart marker ###PRODUCT_ITEMNUMBER_FN_NOT_EMPTY###
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/addons_tt_products/trunk@81724 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Addons to tt_products',
 	'description' => 'Many translations of standard texts for shops. Additional example templates and icons for the tt_products shop system version 2.9+.',
-	'category' => 'example',
+	'category' => 'templates',
 	'author' => 'Franz Holzinger',
 	'author_email' => 'franz@ttproducts.de',
 	'shy' => '',
@@ -29,10 +29,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'ttproducts.org',
-	'version' => '0.3.0',
+	'version' => '0.3.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.3.0-6.1.99',
+			'typo3' => '4.3.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
