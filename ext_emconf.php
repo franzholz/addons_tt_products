8449d4f79cc3b0d254c622743bf3c23962eef731
Franz Holzinger <holzinger@ttproducts.de>
2018-08-20 19:28:37 +0200

    only TYPO3 7.6 and later will be supported by this extension

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,34 +5,34 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Addons to tt_products',
-	'description' => 'Many translations of standard texts for shops. Additional example templates and icons for the tt_products shop system version 2.9 and later.',
-	'category' => 'templates',
-	'author' => 'Franz Holzinger',
-	'author_email' => 'franz@ttproducts.de',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'author_company' => 'jambage.com',
-	'version' => '0.5.4',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '4.5.0-8.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'Addons to tt_products',
+    'description' => 'Many translations of standard texts for shops. Additional example templates and icons for the tt_products shop system version 2.9 and later.',
+    'category' => 'templates',
+    'author' => 'Franz Holzinger',
+    'author_email' => 'franz@ttproducts.de',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'author_company' => 'jambage.com',
+    'version' => '0.6.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-8.99.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
 );
 
 
