3a344ec592b2331be08ab6c2ff219b1b75da9d14
Franz Holzinger <franz.holzinger@festwein.de>
2022-05-31 22:18:37 +0200

    tt_products 2.9 will not work under TYPO3 11

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Addons to tt_products',
-    'description' => 'Many translations of standard texts for shops. Additional example templates and icons for the tt_products shop system version 2.9 and later.',
+    'description' => 'Many translations of standard texts for shops. Additional example templates and icons for the tt_products shop system version 2.14 and later.',
     'category' => 'templates',
     'author' => 'Franz Holzinger',
     'author_email' => 'franz@ttproducts.de',
